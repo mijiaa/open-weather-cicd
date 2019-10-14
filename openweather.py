85acb079fe38445f4495fb2a6f0055d84dbd7c48
Kim <myap0008@student.monash.edu>
2019-10-14 21:49:10 +0800

    Commented out the urllib. Added more unit test case for each data commands and help command.

--- openweather.py
+++ openweather.py
@@ -127,6 +127,7 @@ def check_command_args(command_arg):
                 raise Exception("Multiple chosen data are specified.")
             else:
                 cloud = True
+                check_data_inputs = True
         elif command == '-humidity':
             if humidity:
                 raise Exception("Multiple chosen data are specified.")
@@ -155,6 +156,7 @@ def check_command_args(command_arg):
             # Check if the first argument is -help
             if len(command_arg) == 2:
                 help = True
+                check_data_inputs = True
             else:
                 # -help can't be called with other commands
                 raise IndexError("-help command can only be called alone. ")
@@ -174,101 +176,106 @@ def check_command_args(command_arg):
 
 
 def displaying_message(help, location_check_lst, data_check_lst, user_inputs):
-    try:
-        if help:
-            print("\n-api=<API_key> : where you should put the API key in the <API_key> part. \n" +
-                  "-help can only be called alone, meaning it can't be called with other commands. \n" +
-                  "\nThere are 4 commands to pick the location of the data you want to look into and they are: \n" +
-                  "   -city=<city_name> : the city’s name of that location, \n" +
-                  "   -cid=<city_id> : the city’s id of that location, \n" +
-                  "   -gc=<geographic_coordinates> : the latitude and longitude of that location \n" +
-                  "   -z=<zip_code> : the zip code of that location.\n" +
-                  "\nAfter choosing a location command, you have 8 information commands and they are: \n" +
-                  "   -time : the time when the data was taken, \n" +
-                  "   -temp : the temperature range of the location default is in celsius, if there are no inputs stated. \n" +
-                  "   -pressure : the air pressure in that location, \n" +
-                  "   -cloud :  the number of clouds present at the location, \n" +
-                  "   -humidity: the humidity of the location, \n" +
-                  "   -wind : the wind speed and the angle of the wind, \n" +
-                  "   -sunset : the time the sun settled at the location, \n" +
-                  "   -sunrise : the time the sun rises at that location.")
-            return
+    # try:
+    if help:
+        print("\n-api=<API_key> : where you should put the API key in the <API_key> part. \n" +
+              "-help can only be called alone, meaning it can't be called with other commands. \n" +
+              "\nThere are 4 commands to pick the location of the data you want to look into and they are: \n" +
+              "   -city=<city_name> : the city’s name of that location, \n" +
+              "   -cid=<city_id> : the city’s id of that location, \n" +
+              "   -gc=<geographic_coordinates> : the latitude and longitude of that location \n" +
+              "   -z=<zip_code> : the zip code of that location.\n" +
+              "\nAfter choosing a location command, you have 8 information commands and they are: \n" +
+              "   -time : the time when the data was taken, \n" +
+              "   -temp : the temperature range of the location default is in celsius, if there are no inputs stated. \n" +
+              "   -pressure : the air pressure in that location, \n" +
+              "   -cloud :  the number of clouds present at the location, \n" +
+              "   -humidity: the humidity of the location, \n" +
+              "   -wind : the wind speed and the angle of the wind, \n" +
+              "   -sunset : the time the sun settled at the location, \n" +
+              "   -sunrise : the time the sun rises at that location.")
+        return
 
-        [api_key, loc_data] = user_inputs
+    [api_key, loc_data] = user_inputs
 
-        base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-        if location_check_lst[0]:
-            city_name = loc_data
-            complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-        elif location_check_lst[1]:
-            city_id = loc_data
-            complete_url = base_url + "appid=" + api_key + "&id=" + city_id
-        elif location_check_lst[2]:
-            if "," in loc_data:
-                zip_code = loc_data
-                complete_url = base_url + "appid=" + api_key + "&zip=" + zip_code
-            else:
-                raise ValueError("\nWhen entering the zip code and the country code for this command, separate them with a ','")
-
-        elif location_check_lst[3]:
-            if "," in loc_data:
-                geo = loc_data.split(',')
-                lat = geo[0]
-                lon = geo[1]
-                complete_url = base_url + "appid=" + api_key + "&lat=" + lat + "&lon=" + lon
-            else:
-                raise ValueError("When entering the latitude and longitude coordinates for this command, separate them with a ','")
+    if location_check_lst[0]:
+        city_name = loc_data
+        complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    elif location_check_lst[1]:
+        city_id = loc_data
+        complete_url = base_url + "appid=" + api_key + "&id=" + city_id
+    elif location_check_lst[2]:
+        if "," in loc_data:
+            zip_code = loc_data
+            complete_url = base_url + "appid=" + api_key + "&zip=" + zip_code
         else:
-            # If the user never mentioned any location commands
-            raise Exception("Please enter a location command")
+            raise ValueError("\nWhen entering the zip code and the country code for this command, separate them with a ','")
 
-        response = requests.get(complete_url)
-        json_result = response.json()
-        # response = urllib.request.urlopen(complete_url)
-        # json_result = json.loads(response.read())
+    elif location_check_lst[3]:
+        if "," in loc_data:
+            geo = loc_data.split(',')
+            lat = geo[0]
+            lon = geo[1]
+            complete_url = base_url + "appid=" + api_key + "&lat=" + lat + "&lon=" + lon
+        else:
+            raise ValueError("When entering the latitude and longitude coordinates for this command, separate them with a ','")
+    else:
+        # If the user never mentioned any location commands
+        raise Exception("Please enter a location command")
 
-        [time, temp, temp_data, pressure, cloud, humidity, wind, sunset, sunrise] = data_check_lst
+    response = requests.get(complete_url)
 
-        print("\n  ")
-
-        if time:
-            time_string = get_date_and_time_string(json_result['dt'])
-            print("Time of weather shown is " + time_string)
-        if temp:
-            temp_min = json_result['main']['temp_min']
-            temp_max = json_result['main']['temp_max']
-            if temp_data:
-                celsius_min = str(round(temp_min - 273.15, 2))
-                celsius_max = str(round(temp_max - 273.15, 2))
-                print("The temperature ranges from " + celsius_min + " to " + celsius_max + " celsius. ")
-            else:
-                fahrenheit_min = str(round((temp_min / 273.15) * 9 / 5 + 32, 2))
-                fahrenheit_max = str(round((temp_max / 273.15) * 9 / 5 + 32, 2))
-                print("The temperature ranges from " + str(fahrenheit_min) + " to " + str(fahrenheit_max) + " fahrenheit. ")
-        if pressure:
-            pressure_result = json_result['main']['pressure']
-            print("The atmospheric pressure is " + str(pressure_result) + "hPa. ")
-        if cloud:
-            description = json_result['weather'][0]['description']
-            cloudiness = json_result['clouds']['all']
-            print("It is likely to be " + str(description) + " with a cloudiness of " + str(cloudiness) + "%. ")
-        if humidity:
-            description =  json_result['weather'][0]['description']
-            humidity_percent = json_result['clouds']['all']
-            print("It is likely to be " + str(description) + " with a humidity of " + str(humidity_percent) + "%. ")
-        if wind:
-            wind_speed = json_result['wind']['speed']
-            wind_angle = json_result['wind']['deg']
-            print("A wind speed of " + str(wind_speed) + "m/s from " + str(wind_angle) + " degrees. ")
-        if sunset:
-            time_string = get_time_string(json_result['sys']['sunset'])
-            print("The sun sets at " + time_string)
-        if sunrise:
-            time_string = get_time_string(json_result['sys']['sunrise'])
-            print("The sun rises at " + time_string)
-    except HTTPError:
+    if response.status_code == 404:
         raise ValueError("Entered in wrong inputs given to the commands.")
+
+    json_result = response.json()
+
+    # response = urllib.request.urlopen(complete_url)
+    # json_result = json.loads(response.read())
+
+    [time, temp, temp_data, pressure, cloud, humidity, wind, sunset, sunrise] = data_check_lst
+
+    print("\n  ")
+
+    if time:
+        time_string = get_date_and_time_string(json_result['dt'])
+        print("Time of weather shown is " + time_string)
+    if temp:
+        temp_min = json_result['main']['temp_min']
+        temp_max = json_result['main']['temp_max']
+        if temp_data:
+            celsius_min = str(round(temp_min - 273.15, 2))
+            celsius_max = str(round(temp_max - 273.15, 2))
+            print("The temperature ranges from " + celsius_min + " to " + celsius_max + " celsius. ")
+        else:
+            fahrenheit_min = str(round((temp_min / 273.15) * 9 / 5 + 32, 2))
+            fahrenheit_max = str(round((temp_max / 273.15) * 9 / 5 + 32, 2))
+            print("The temperature ranges from " + str(fahrenheit_min) + " to " + str(fahrenheit_max) + " fahrenheit. ")
+    if pressure:
+        pressure_result = json_result['main']['pressure']
+        print("The atmospheric pressure is " + str(pressure_result) + "hPa. ")
+    if cloud:
+        description = json_result['weather'][0]['description']
+        cloudiness = json_result['clouds']['all']
+        print("It is likely to be " + str(description) + " with a cloudiness of " + str(cloudiness) + "%. ")
+    if humidity:
+        description =  json_result['weather'][0]['description']
+        humidity_percent = json_result['clouds']['all']
+        print("It is likely to be " + str(description) + " with a humidity of " + str(humidity_percent) + "%. ")
+    if wind:
+        wind_speed = json_result['wind']['speed']
+        wind_angle = json_result['wind']['deg']
+        print("A wind speed of " + str(wind_speed) + "m/s from " + str(wind_angle) + " degrees. ")
+    if sunset:
+        time_string = get_time_string(json_result['sys']['sunset'])
+        print("The sun sets at " + time_string)
+    if sunrise:
+        time_string = get_time_string(json_result['sys']['sunrise'])
+        print("The sun rises at " + time_string)
+    # except:
+    #     raise ValueError("Entered in wrong inputs given to the commands.")
 
 
 def get_date_and_time_string(seconds):
--- openweather_unittest.py
+++ openweather_unittest.py
@@ -3,13 +3,92 @@ import unittest
 
 
 class openWeatherTests(unittest.TestCase):
+    # Check if the -time command is functioning
     def test_time_command(self):
         commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-time']
-        result = check_command_args(['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-time'])
+        result = check_command_args(commands)
 
         actual_result = [False, [True, False, False, False], [True, False, True, False, False, False, False, False, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
         self.assertEqual(result, actual_result, "The checking of the -time is wrong.")
 
+    # Check if the default of the -temp command is functioning
+    def test_temp_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-temp']
+        result = check_command_args(commands)
+
+        actual_result = [False, [True, False, False, False], [False, True, True, False, False, False, False, False, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -temp is wrong.")
+
+    # Check if the default of the -temp command is functioning
+    def test_temp_celsius_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-temp=celsius']
+        result = check_command_args(commands)
+
+        actual_result = [False, [True, False, False, False], [False, True, True, False, False, False, False, False, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -temp command with the input of celsius is wrong.")
+
+    # Check if the default of the -temp command is functioning
+    def test_temp_fahrenheit_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-temp=fahrenheit']
+        result = check_command_args(commands)
+
+        actual_result = [False, [True, False, False, False], [False, True, False, False, False, False, False, False, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -temp command with the input of fahrenheit is wrong.")
+
+    # Check if the default of the -temp command is functioning
+    def test_temp_random_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-temp=fahrenheit']
+        result = check_command_args(commands)
+        # ???
+        actual_result = [False, [True, False, False, False], [False, True, False, False, False, False, False, False, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -temp command with the input of fahrenheit is wrong.")
+
+    # Check if the default of the -pressure command is functioning
+    def test_pressure_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-pressure']
+        result = check_command_args(commands)
+
+        actual_result = [False, [True, False, False, False], [False, False, True, True, False, False, False, False, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -pressure is wrong.")
+
+    # Check if the default of the -cloud command is functioning
+    def test_cloud_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-cloud']
+        result = check_command_args(commands)
+
+        actual_result = [False, [True, False, False, False], [False, False, True, False, True, False, False, False, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -cloud is wrong.")
+
+    # Check if the default of the -humidity command is functioning
+    def test_humidity_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-humidity']
+        result = check_command_args(commands)
+
+        actual_result = [False, [True, False, False, False], [False, False, True, False, False, True, False, False, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -humidity is wrong.")
+
+    # Check if the default of the -sunset command is functioning
+    def test_sunset_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-sunset']
+        result = check_command_args(commands)
+
+        actual_result = [False, [True, False, False, False], [False, False, True, False, False, False, False, True, False], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -sunset is wrong.")
+
+    # Check if the default of the -sunrise command is functioning
+    def test_sunrise_command(self):
+        commands = ['openweather.py', '-api=170dae04cac7827d30fd3679c496ffb4', '-city=London', '-sunrise']
+        result = check_command_args(commands)
+
+        actual_result = [False, [True, False, False, False], [False, False, True, False, False, False, False, False, True], ['170dae04cac7827d30fd3679c496ffb4', 'London']]
+        self.assertEqual(result, actual_result, "The checking of the -sunrise is wrong.")
+
+    def test_help_command(self):
+        commands = ['openweather.py', '-help']
+        result = check_command_args(commands)
+
+        actual_result = [True, [False, False, False, False], [False, False, True, False, False, False, False, False, False], [None, None]]
+        self.assertEqual(result, actual_result, "The checking of the -help is wrong.")
 
 if __name__ == "__main__":
     unittest.main()
